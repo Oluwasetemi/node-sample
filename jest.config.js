b06e51268b84a18310b85a7798105b9a9891f41b
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-11-08 01:54:58 +0100

    📦 NEW: improve the config.yml file to try artifact example

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     ...require('./src/tests/jest-common'),
     projects: ['./src/tests/jest.lint.js', './src/tests/jest.server.js'],
+    coverageDirectory: '../coverage',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
 };
